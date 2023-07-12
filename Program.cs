e294f107fc3497334d85d9dedf2b2fc3b91846eb
vk1345 <139271191+vk1345@users.noreply.github.com>
2023-07-12 09:42:53 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#include <iostream>
+
+int main() {
+    std::cout << "all is okey";
+
+
+    return 0
+        }
